b0885b56ccb1e4e9d50c183368658872ad83ad66
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-12 21:05:25 -0300

    [configs iniciais]
    eslint

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content type': 'application.json' });
-    res.end(JSON.stringify({
-        data: 'Fala, levs'
-    }));
-})
-.listen(4001, console.log("Porta: 4001"));
