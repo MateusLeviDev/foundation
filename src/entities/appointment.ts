f4ffce16485e10ead1a896401efe190647eff844
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-14 22:06:32 -0300

    [vitest/config]

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,5 +1,26 @@
 // padrão de classes
-class Appointment {
-  private startAt: Date;
-  private endAt: Date;
+interface AppointmentProps {
+  customer: string;
+  statAt: Date;
+  endAt: Date;
+}
+
+export class Appointment {
+  private props: AppointmentProps;
+
+  get customer() {
+    return this.props.customer;
+  }
+
+  get startAt() {
+    return this.props.statAt;
+  }
+
+  get endAt() {
+    return this.props.endAt;
+  }
+
+  constructor(props: AppointmentProps) {
+    this.props = props;
+  }
 }
