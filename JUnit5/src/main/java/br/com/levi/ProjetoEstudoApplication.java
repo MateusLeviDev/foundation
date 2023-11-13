f660c2845bee95481579e408f30c7e774a1f2a6e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-13 18:23:56 -0300

    feat: added junit jupyter code examples

---
+++ JUnit5/src/main/java/br/com/levi/ProjetoEstudoApplication.java
@@ -0,0 +1,8 @@
+package br.com.levi;
+
+public class ProjetoEstudoApplication {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JUnit5/src/test/java/br/com/levi/AppTest.java
@@ -0,0 +1,14 @@
+package br.com.levi;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    void test() {
+        System.out.println("jbcbsccoij");
+    }
+}
