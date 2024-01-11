6e5445de896bbd73f8b77c49574346a62aab1571
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-01-10 23:37:50 -0300

    Initial commit

---
+++ oops-concepts/src/main/java/com/levi/oops/concepts/OopsConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.levi.oops.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsConceptsApplication.class, args);
+	}
+
+}
---
+++ oops-concepts/src/test/java/com/levi/oops/concepts/OopsConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.oops.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopsConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
