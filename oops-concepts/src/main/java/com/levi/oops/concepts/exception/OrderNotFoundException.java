9d9c48e11c91dff0faefb429c6791d00e1d04c5b
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-01-17 20:57:00 -0300

    `feat`: Compile time exceptions can be handled at compile time. Understanding the order of exception hierarchy is important while handling exceptions in Java.

---
+++ oops-concepts/src/main/java/com/levi/oops/concepts/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.levi.oops.concepts.exception;
+
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
