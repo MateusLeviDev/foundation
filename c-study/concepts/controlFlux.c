9c46df60f878b7a66cfd9e1593eb5c4a6a697885
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-01-27 22:39:03 -0300

    feat: added new exercises using bitwise operators

--- c-study/concepts/controlFlux.c
+++ c-study/concepts/controlFlux.c
@@ -17,7 +17,14 @@ int main() {
     (hoursWorked * PAY_RATE) : 
     (OVERTIME_THRESHOLD * PAY_RATE + (hoursWorked - OVERTIME_THRESHOLD) * PAY_RATE * 1.5);
 
-    printf("%.5f\n", grossPay);
+    taxes = (grossPay <= 300) ? grossPay * RATE_1 : 
+    (grossPay <= 450) ? 300 * RATE_1 + (grossPay - 300) * RATE_2 : 
+    300 * RATE_1 + 150 * RATE_2 + (grossPay - 450) * RATE_3;
 
-    //TODO: .....
+    netPay = grossPay - taxes;
+
+    printf("\nSalário Bruto: $%.2f\n", grossPay);
+    printf("Impostos: $%.2f\n", taxes);
+    printf("Salário Líquido: $%.2f\n", netPay);
+    
 }
