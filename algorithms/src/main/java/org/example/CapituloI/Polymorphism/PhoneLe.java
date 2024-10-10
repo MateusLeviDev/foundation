55c02947df0554a562d3bece8866791d1e77b30a
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-10-09 21:22:07 -0300

    rebuilding repository - Add JUnit5 first project template

--- algorithms/src/main/java/org/example/CapituloI/Polymorphism/PhoneLe.java
+++
@@ -1,8 +0,0 @@
-package org.example.CapituloI.Polymorphism;
-
-public class PhoneLe implements Payment{
-    @Override
-    public void doTransaction() {
-        System.out.println("PhoneLe transaction");
-    }
-}
--- oops-concepts/src/main/java/com/levi/oops/concepts/service/ClassB.java
+++
@@ -1,8 +0,0 @@
-package com.levi.oops.concepts.service;
-
-public class ClassB extends ClassA{
-
-    //if you are returning any parent object as a return type in your interface
-    //or class the in your implementation you can return child object of it
-
-}
